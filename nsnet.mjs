ba192251006363adc4df60b9701be1d4ff03b709
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-10-12 23:32:21 +0300

    Fixed Puppeteer Chrome path bug

--- nsnet.mjs
+++ nsnet.mjs
@@ -26,6 +26,7 @@ app.post('/process', async (req, res) => {
     try {
         const browser = await puppeteer.launch({
             headless: true,
+            executablePath: '/usr/bin/google-chrome',
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
         });
 
